8628ae27a53c2939b1e500a58af87061490076fc
Jacob Ginsparg <jacobginsparg@gmail.com>
2023-07-07 19:22:28 -0400

    Crude image search

--- src/components/imageSearch.tsx
+++ src/components/imageSearch.tsx
@@ -1,8 +1,79 @@
+import React, { useState } from "react"
+import { PIXABAY_API_KEY, PIXABAY_BASE_URL } from "../constants"
+
+type PixabayImageJSON = {
+    id: number,
+	pageURL: string,
+	type: string,
+	tags: string,
+	previewURL: string,
+	previewWidth: number,
+	previewHeight: number,
+	webformatURL: string,
+	webformatWidth: number,
+	webformatHeight: number,
+	largeImageURL: string,
+	fullHDURL: string,
+	imageURL: string,
+	imageWidth: number,
+	imageHeight: number,
+	imageSize: number,
+	views: number,
+	downloads: number,
+	likes: number,
+	comments: number,
+	user_id: number,
+	user: string,
+    userImageURL: string,
+}
+
+type PixabayResponse = {
+    total: number,
+    totalHits: number,
+    hits: PixabayImageJSON[],
+}
+
+function getNiceImages(responseText: string): PixabayImageJSON[] {
+    console.log(responseText);
+    
+    const responseJSON: PixabayResponse = JSON.parse(responseText)
+    return responseJSON.hits
+}
+
 export default function ImageSearch() {
+    const [images, setImages] = useState<PixabayImageJSON[]>([])
+    const [showImageDetails, setShowImageDetails] = useState<Boolean>(false)
+
+    function handleChange(e: React.FormEvent<HTMLInputElement>) {
+        e.preventDefault()
+
+        fetch(PIXABAY_BASE_URL + new URLSearchParams({
+            key: PIXABAY_API_KEY,
+            q: e.currentTarget.value,
+        }))
+        .then((response) => response.text())
+        .then((responseText) => setImages(getNiceImages(responseText)))
+    }
+
+    const imageListItems = images ? images.map(image => 
+        <li className='list-image-none' key={image.id}>
+            <img src={image.previewURL} />
+        </li>
+    ) : <label></label>
+
     return (
         <>
             <div>
-                Image Search
+                <label className='underline'>Image Search</label>
+                <input
+                    type='search'
+                    id='imageSearchQuery'
+                    placeholder='What kind of image are you looking for?'
+                    onChange={handleChange}
+                />
+                <ul className='list-image-none'>
+                    {imageListItems}
+                </ul>
             </div>
         </>
     )
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const PIXABAY_API_KEY = '38128497-01d0f2e4fa9b07eb8fbc55624'
+export const PIXABAY_BASE_URL = 'https://pixabay.com/api/?'
