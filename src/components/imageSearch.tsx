ae6798bb3c0b5154a5d142c8a4b928c25ffb40c8
Jacob Ginsparg <jacobginsparg@gmail.com>
2023-07-07 20:09:52 -0400

    Crude details page

--- src/components/imageSearch.tsx
+++ src/components/imageSearch.tsx
@@ -33,47 +33,72 @@ type PixabayResponse = {
     hits: PixabayImageJSON[],
 }
 
-function getNiceImages(responseText: string): PixabayImageJSON[] {
-    console.log(responseText);
-    
-    const responseJSON: PixabayResponse = JSON.parse(responseText)
-    return responseJSON.hits
-}
-
 export default function ImageSearch() {
     const [images, setImages] = useState<PixabayImageJSON[]>([])
     const [showImageDetails, setShowImageDetails] = useState<Boolean>(false)
+    const [modalImage, setModalImage] = useState<PixabayImageJSON>()
 
     function handleChange(e: React.FormEvent<HTMLInputElement>) {
         e.preventDefault()
-
         fetch(PIXABAY_BASE_URL + new URLSearchParams({
             key: PIXABAY_API_KEY,
             q: e.currentTarget.value,
         }))
         .then((response) => response.text())
-        .then((responseText) => setImages(getNiceImages(responseText)))
+        .then((responseText) => {
+            setImages((JSON.parse(responseText) as PixabayResponse).hits)
+        })
+    }
+
+    function openModal(image: PixabayImageJSON) {
+        return (e: React.MouseEvent<HTMLImageElement>) => {
+        e.preventDefault()
+        setShowImageDetails(true)
+        setModalImage(image)
+    }}
+
+    function closeModal(e: React.MouseEvent<HTMLButtonElement>) {
+        e.preventDefault()
+        setShowImageDetails(false)
     }
 
     const imageListItems = images ? images.map(image => 
         <li className='list-image-none' key={image.id}>
-            <img src={image.previewURL} />
+            <img src={image.previewURL} onClick={openModal(image)} />
         </li>
     ) : <label></label>
+
+    const imageDetailsModal = (
+        <div>
+            <img src={modalImage?.webformatURL} />
+            <p>
+                This image was posted by {modalImage?.user}
+                <br />
+                Tags: {modalImage?.tags}
+            </p>
+            <button onClick={closeModal}>Close details</button>
+        </div>
+    )
 
     return (
         <>
             <div>
-                <label className='underline'>Image Search</label>
-                <input
-                    type='search'
-                    id='imageSearchQuery'
-                    placeholder='What kind of image are you looking for?'
-                    onChange={handleChange}
-                />
-                <ul className='list-image-none'>
-                    {imageListItems}
-                </ul>
+                <h3 className='underline'>Image {showImageDetails ? 'Details' : 'Search'}</h3>
+                {showImageDetails ?
+                imageDetailsModal : 
+                (
+                    <>
+                        <input
+                            type='search'
+                            id='imageSearchQuery'
+                            placeholder='What kind of image are you looking for?'
+                            onChange={handleChange}
+                        />
+                        <ul className='list-image-none'>
+                            {imageListItems}
+                        </ul>
+                    </>
+                )}
             </div>
         </>
     )
